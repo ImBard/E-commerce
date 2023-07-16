fb6d66941bf0a234581dbda71eb613ede86a5210
imbard <nicota1234@gmail.com>
2023-07-16 03:55:15 -0300

    Login responsivo

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,50 +1,57 @@
-import { Actions, Check, Content, Cover, Forgot, Form, ImgLogo, Input, InputLabel, LabelCheck, Logo, Main, Register, SubTitle, SubmitButton, Title } from "./style";
+import { Actions, Check, Container, Content, Cover, Doogle, Forgot, Form, ImgLogo, Input, InputLabel, LabelCheck, Logo, Main, Register, SubTitle, SubmitButton, Title } from "./style";
 import LogoImg from "../../assets/logo.png";
 
 export function Login() {
 
   return (
-    <Main>
-      <Form>
-        <Logo>
-          <ImgLogo src={LogoImg} />
-          prada venice
-        </Logo>
-        <Content>
-          <Title>
-            Welcome back!
-          </Title>
-          <SubTitle>The Faster you fill up, the faster you get your products</SubTitle>
+    <>
+      <Main>
+        <Container>
+          <Form>
+            <Logo>
+              <ImgLogo src={LogoImg} />
+              prada venice
+            </Logo>
+            <Content>
+              <Title>
+                Welcome back!
+              </Title>
+              <SubTitle>The Faster you fill up, the faster you get your products</SubTitle>
 
-          <InputLabel htmlFor="email">
-            Email
-            <Input type="email" id="email" placeholder="Enter your email" />
-          </InputLabel>
+              <InputLabel htmlFor="email">
+                Email
+                <Input type="email" id="email" placeholder="Enter your email" />
+              </InputLabel>
 
-          <InputLabel htmlFor="password">
-            Password
-            <Input type="password" id="password" placeholder="*******"/>
-          </InputLabel>
+              <InputLabel htmlFor="password">
+                Password
+                <Input type="password" id="password" placeholder="*******" />
+              </InputLabel>
 
-          <Actions>
-            <LabelCheck htmlFor="check">
-              <Check type="checkbox" id="check"/>
-              Remember me
-            </LabelCheck>
-            <Forgot to="/">Forgot Password</Forgot>
-          </Actions>
+              <Actions>
+                <LabelCheck htmlFor="check">
+                  <Check type="checkbox" id="check" />
+                  Remember me
+                </LabelCheck>
+                <Forgot to="/">Forgot Password</Forgot>
+              </Actions>
 
-          <SubmitButton>Sign In</SubmitButton>
+              <SubmitButton>Sign In</SubmitButton>
+              <Doogle>Sign In</Doogle>
 
-        </Content>
-        <Register to="/">
+            </Content>
+            <Register to={"/signup"}>
+              {/* eslint-disable-next-line react/no-unescaped-entities */}
+              Don't have an account? <b>Sign up</b>
+            </Register>
+          </Form>
+          <Cover>
+            asdasdasdasdasda
+          </Cover>
 
-        </Register>
-      </Form>
-      <Cover>
-asdasdasdasdasda
-      </Cover>
-    </Main>
+        </Container>
+      </Main>
+    </>
 
   );
 }
--- src/pages/login/style.js
+++ src/pages/login/style.js
@@ -5,13 +5,42 @@ export const Main = styled.div`
   display: flex;
   flex-direction: column;
   min-height: 100vh;
+  width: 100%;
+  @media only screen and (min-width: 750px) {
+    justify-content: center;
+    align-items: center;
+    background-color: #e6e6e6;
+  }
 
+`;
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  @media only screen and (min-width: 750px) {
+    flex-direction: row;
+    width: 80%;
+    justify-content: center;
+
+    background-color: white;
+    border-radius: 8px;
+    box-shadow: 4px 4px 4px rgba(0,0,0,0.2);
+  }
 `;
 
 export const Form = styled.form`
   padding: 21px;
   /* margin: auto; */
   height: 100vh;
+
+  @media only screen and (min-width: 750px) {
+    width: 70%;
+    margin-right: 50px;
+    padding: 3% 5% 20px 5%;
+    height: 100%;
+  }
+
 `;
 
 export const Logo = styled.div`
@@ -100,13 +129,37 @@ export const SubmitButton = styled.button`
   margin-top: 34px;
 `;
 
-export const Register = styled(Link)``;
+export const Doogle = styled.button`
+border: none;
+outline: none;
+background-color: black;
+color: white;
+font-weight: bold;
+padding: 13px;
+font-size: medium;
+margin-top: 13px;
+`;
+
+export const Register = styled(Link)`
+  text-align: center;
+  text-decoration: none;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 50px;
+`;
 
 export const Cover = styled.div`
-  background-color: red;
+  background-image: url("https://github.com/ImBard/E-commerce/blob/main/src/assets/imgModels/prada1.jpg?raw=true");
+  object-fit: cover;
+  object-position: top;
+  width: 100%;
+  min-height: 650px;
+  background-repeat: no-repeat;
 
   @media only screen and (min-width: 750px) {
-    display: none;
-    background-color: blue;
+    display: flex;
+    width: 100%;
   }
 `;
